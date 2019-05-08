72c88f506b974ed11c1919cb6cd256e526e192db
Ben Tupper <btupper@bigelow.org>
2019-05-08 10:18:14 -0400

    move tes scripts out, fix readme

--- inst/rtofs-gom.R
+++
@@ -1,8 +0,0 @@
-
-params = c("u_velocity" = "ucv",
-           "v_velocity" = "vcv",
-           "sst"  = "sst",
-           "sss" = "sss",
-           "ssh" = "ssh",
-           "ice_coverage" = "sie",
-           "ice_thickness" = "sit")
